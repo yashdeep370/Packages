2f2c1d042f2923049dc9775de697e30c5835cef8
Yash Deep <143354431+yashdeep370@users.noreply.github.com>
2025-01-10 19:40:43 +0530

    Add files via upload

---
+++ strpackages.java
@@ -0,0 +1,9 @@
+// import java.lang.*;
+
+class strpackages{
+    public static void main(String[] args) {
+        java.lang.String str;
+        str = new java.lang.String("Hello");
+        System.out.println(str);
+    }
+}
