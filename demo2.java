d9b149fb18816e0a28cdc74ce6ea96497fd625c2
Yash Deep <143354431+yashdeep370@users.noreply.github.com>
2025-01-11 01:53:51 +0530

    Add files via upload

---
+++ demo2.java
@@ -0,0 +1,9 @@
+package mypack1;
+
+public class demo2
+{
+    public void display()
+    {
+        System.out.println("demo");
+    }
+}
---
+++ demo3.java
@@ -0,0 +1,9 @@
+package mypack1.inner;
+
+public class demo3
+{
+    public void display()
+    {
+        System.out.println("demo");
+    }
+}
